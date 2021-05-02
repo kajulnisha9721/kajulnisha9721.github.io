2a3a1ebc1666d97687ce1d45f51763619fc696f9
Kajul Nisha <83535981+kajulnisha9721@users.noreply.github.com>
2021-05-02 16:16:41 +0530

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+				hidevalidation();
+				$('#contactform')[0].reset(); //form should be reset everytime on page load
+				document.getElementById("yearid").innerHTML = new Date().getFullYear();
+				$('#contactPh').keypress(function (event) {
+    				var keycode = event.which;
+    				if (!(event.shiftKey == false && (keycode == 46 || keycode == 8 || keycode == 37 || keycode == 39 || (keycode >= 48 && keycode <= 57)))) {
+        				event.preventDefault();
+    				}
+				});
+				$("#formsubmitcontact").click(function(){					
+					hidevalidation();
+					if(validation()==false)
+					{						
+							var link = "mailto:kajulnisha.umar9721@gmail.com"
+									//+ "?cc=myCCaddress@example.com"
+									+"?From="+ encodeURIComponent(document.getElementById('contactPh').value)
+								+ "&subject=" + encodeURIComponent("Blog Mail from : "+ document.getElementById('contactname').value +"/"+document.getElementById('contactPh').value)
+								+ "&body=" + encodeURIComponent(document.getElementById('contactmessage').value);
+	
+							window.location.href = link;
+					}else{
+						alert("Please fill the required fields");
+					}
+					
+				});
+				$('#resetform,.close').click(function(){  					
+					$('#contactform')[0].reset();
+				});
+				function validation(){
+					var flag=0;
+					if($("#contactname").val()==0){
+						//$('#spancontactname').show();
+						flag+=1;
+					}
+					if($("#contactPh").val()==0){
+						//$('#spancontactcontactPh').show();
+						flag+=1;
+					}
+					if($("#contactmessage").val()==0){
+						//$('#spancontactmessage').show();
+						flag+=1;
+					}
+
+					if(flag==0){						
+						return false;
+					}
+					else{						
+						return true;
+					}
+
+				}
+				function hidevalidation(){
+					$('#spancontactname').css('display','none');
+					$('#spancontactPh').css('display','none');
+					$('#spancontactmessage').css('display','none');
+				}
+});
